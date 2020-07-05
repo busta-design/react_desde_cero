4f458a2b37bd0f811bf6e4319c7ce9263322318d
Andres <andresbusta4444@gmail.com>
2020-07-05 13:39:10 -0400

    Lo que sea

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import './styles/styles.scss'
+import Curso from './Curso';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+    <div className="main-banner img-container l-section" id="main-banner">
+      <div className="ed-grid lg-grid-6">
+        <div className="lg-cols-4 lg-x-2">
+          <img className="main-banner__img" alt="banner" src="https://images.unsplash.com/photo-1515248027005-c33283ec3fba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"/>
+          <div className="main-banner__data s-center">
+            <p className="t2 s-mb-0">Cursos de EDteam</p>
+            <p> Tu futuro te esta esperando</p>
+            <a href="https://ed.team" className="button">Suscribirse</a>
+          </div>
+        </div>
+      </div>
     </div>
-  );
-}
 
+    <div className="ed-grid m-grid-3">
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      
+    </div>
+  </>
+)
 export default App;
